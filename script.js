b18d20f44b127ed6e36bb1f925d75e43be4d3dca
levangorozia <106963464+levangorozia@users.noreply.github.com>
2022-08-05 16:03:12 +0400

    add davalebebi

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
 // console.log(array.length);.
 
 
-// davaleba #4.  მოცემულია მასივი:
+// davaleba #4.  
 // filter-ის გამოყენებით გაფილთრეთ და გამოიტანეთ მარტო ციფრები; 
 
 // varianiti #1
@@ -42,3 +42,43 @@
 // let newarray = array.filter ((item)  => typeof item == 'number');
 
 // console.log(newarray);
+
+
+
+// davaleba #5. ფილტრის საშულებით გაფილტრეთ და გამოიტანეთ მარტო ის სიტყვები რომლების სიმბოლოების რაოდენობა იქნება 3-ზე მეტი;
+
+
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+
+// let languagesnewarray = languages.filter (y => y.length >3);
+
+// console.log(languagesnewarray);
+
+
+// davaleba #6. Reduce-ის საშუალებით მიიღეთ ერთი კომბინირებული სტრინგი(academy of digital industries);
+
+// let array= ['academy', 'of', 'digital', 'industries'].reduce ( (accumulator, currentvalue) => 
+//     accumulator.concat ( ' ' + currentvalue)
+// )
+
+// console.log(array);
+
+
+
+// #7. davaleba შექმენით ახალი მასივი map ფუნქციის გამოყენებით შემდეგი ლოგიკით: 
+// თუ ელემენტი არის რიცხვი ამ მასივში უნდა იყოს ამ რიცხვის კვადრატი. 
+// თუ ელემენტი არის ტექსტი, მაშინ ახალ მასივში უნდა იყოს ეს ტექსტი დიდი ასოებით. სხვა შემთხვევაში ელემენტები უნდა დაემატოს უცვლელად;
+
+// let item = [12, 'google', 32, null, 'yahoo', 25];
+
+// let result = item.map (item =>{
+//     if (typeof item == 'number'){
+//         return item * item;
+//     }else if ( typeof item == 'string'){
+//         return item.toUpperCase ();
+//     } return item;
+    
+// })
+
+
+//  console.log(result);
